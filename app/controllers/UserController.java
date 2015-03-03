5ddfd826f008ba7d8ef54ac27613accee190b4dd
Davor Stanković <dastko@gmail.com>
2015-03-03 15:56:41 +0100

    first commit

---
+++ app/controllers/UserController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import models.*;
+import play.mvc.*;
+import views.html.*;
+import play.*;
+import play.data.Form;
+import play.api.db.*;
+
+public class UserController extends Controller {
+	
+	static Form<User> userForm = new Form<User>(User.class);
+	
+	public static Result create(){
+		Form<User> filled = userForm.bindFromRequest();
+		if(!filled.hasErrors()){
+			String email = filled.get().email;
+			String password = filled.get().password;
+			long id = User.createUser(email, password);
+			return redirect("/user/" + id);
+		}
+		return ok(signup.render(userForm));
+	}
+	
+	public static Result newUser(){
+		return ok(signup.render(userForm));
+	}
+	
+	@Security.Authenticated(Session.class)
+	public static Result show(long id){
+		User u = User.find(id);
+		return ok(showUser.render(u));
+	}
+
+}
